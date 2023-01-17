0081a68956ed1917543dd31e0183fc07b43d0ac1
Foma Tuturov <fomiash@yandex.ru>
2023-01-17 20:54:52 +0300

    add create table sql

---
+++ migration_table.sql
@@ -0,0 +1,3 @@
+/* MySQL/MariaDB/PostgreSQL */
+
+CREATE TABLE IF NOT EXISTS migrations (label bigint NOT NULL, datecreate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
